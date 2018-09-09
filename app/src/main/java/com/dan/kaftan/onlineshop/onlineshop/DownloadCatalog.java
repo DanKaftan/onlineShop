a483499b2cd5c5944b80672c72876f4bba622f2e
DanKaftan <dan.kaftan@gmail.com>
2018-09-10 02:35:58 +0300

    removed un used activities

--- app/src/main/java/com/dan/kaftan/onlineshop/onlineshop/DownloadCatalog.java
+++ app/src/main/java/com/dan/kaftan/onlineshop/onlineshop/DownloadCatalog.java
@@ -2,8 +2,15 @@ package com.dan.kaftan.onlineshop.onlineshop;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
@@ -35,12 +42,15 @@ public class DownloadCatalog extends AppCompatActivity {
         // init specific shop params
         ShopSpecificInfo.setShopInfo("shopBucket", "DemoShop");
         ShopSpecificInfo.setShopInfo("catalogFile","catalog.txt");
-        setContentView(R.layout.activity_get_image);
+        setContentView(R.layout.activity_list_view);
 
-        downloadTheCatalog();
+        final android.widget.ListView listView =(android.widget.ListView)findViewById(R.id.listView);
+        DownloadCatalog.CustomAdapter customAdapter = new DownloadCatalog.CustomAdapter();
+
+        downloadTheCatalog(listView, customAdapter);
     }
 
-    private void downloadTheCatalog() {
+    private void downloadTheCatalog(final android.widget.ListView listView, final DownloadCatalog.CustomAdapter customAdapter) {
 
         // get catalog file from storage
         storageRef.child(ShopSpecificInfo.getShopInfo("shopBucket") +"/"+ ShopSpecificInfo.getShopInfo("catalogFile")).getBytes(Long.MAX_VALUE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
@@ -78,9 +88,7 @@ public class DownloadCatalog extends AppCompatActivity {
                     }
 
                     // now that we have the list of items from the catalog, pass it on to the next activity
-                    Intent i = new Intent(DownloadCatalog.this, com.dan.kaftan.onlineshop.onlineshop.ListView.class);
-                    i.putExtra("items", items);
-                    startActivity(i);
+                    listView.setAdapter(customAdapter);
 
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -89,5 +97,57 @@ public class DownloadCatalog extends AppCompatActivity {
             }
         });
 
+    }
+
+    class CustomAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+            view = getLayoutInflater().inflate(R.layout.customlayout,null);
+
+            ImageView imageView = (ImageView)view.findViewById(R.id.item_iv);
+            TextView itemName = (TextView)view.findViewById(R.id.tv_name);
+            TextView itemPrice = (TextView)view.findViewById(R.id.tv_price);
+
+            itemName.setText(items.get(i).getName());
+            itemPrice.setText("מחיר:"+(items.get(i).getPrice()));
+            downloadImage(ShopSpecificInfo.getShopInfo("shopBucket"), items.get(i).getItemImageFile(), imageView);
+
+            return view;
+        }
+    }
+
+    private void downloadImage(String folder, String imageName, final ImageView imageView) {
+
+        System.out.println("going to download: " + folder + "/" +imageName);
+        storageRef.child(folder + "/" +imageName).getBytes(Long.MAX_VALUE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+
+                try {
+                    Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    imageView.setImageBitmap(bmp);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 }
