fb094ae2da6acf2e6fb758dc74dd5177954bfd41
DanKaftan <dan.kaftan@gmail.com>
2018-09-09 23:30:14 +0300

    first commit

--- app/src/main/java/com/dan/kaftan/onlineshop/onlineshop/ListView.java
+++ app/src/main/java/com/dan/kaftan/onlineshop/onlineshop/ListView.java
@@ -39,7 +39,7 @@ public class ListView extends AppCompatActivity {
 
     class CustomAdapter extends BaseAdapter {
 
-//check
+//check1
         @Override
         public int getCount() {
             return items.size();
